360d0a59d870bd6c3e8b1cc6902d26c716aec68e
Eduardo Tenório <embatbr@gmail.com>
2016-07-04 16:11:46 -0300

    Simplification of tests and domain codes. No need for lots of validations, since this is done previously in the app flux.
    Signed-off-by: Eduardo Tenório <embatbr@gmail.com>

--- services/business/app/domains.py
+++ services/business/app/domains.py
@@ -17,33 +17,14 @@ class ItemError(Exception):
 class Item(object):
 
     def __init__(self, sku, name, price):
-        if (not sku) or (not isinstance(sku, str)):
-            raise ItemError('`sku` must be a string')
+        """Type and "None" validation are executed in the module `logic`.
+        """
+        if not re.match('^[0-9]+\.[0-9]{2}$', price):
+            raise ItemError('Amount %s is not in the correct format.' % price)
 
         self.sku = sku.strip()
-
-        if (not name) or (not isinstance(name, str)):
-            raise ItemError('`name` must be a string')
-
         self.name = name.strip()
-
-        currency = price[0]
-        if (not currency) or (not isinstance(currency, str)):
-            raise ItemError('`price[0]` must be a string')
-
-        currency = currency.strip().upper()
-
-        amount = price[1]
-        if (not amount) or (not isinstance(amount, str)):
-            raise ItemError('`price[1]` must be a string')
-
-        amount = amount.strip()
-
-        amount_regex = '^[0-9]+\.[0-9]{2}$'
-        if not re.match(amount_regex, amount):
-            raise ItemError('Amount %s is not in the correct format.' % amount)
-
-        self.price = Money(amount, currency)
+        self.price = Money(price, 'USD')
 
     def describe(self):
         return '%s %s $%s' % (self.sku, self.name, self.price.amount)
@@ -58,40 +39,21 @@ class PurchaseError(Exception):
 class Purchase(object):
 
     def __init__(self, item, amount, timestamp):
-        if (not item) or (not isinstance(item, Item)):
-            raise PurchaseError('`item` must be of type `Item`.')
-
-        if (amount is None) or (not isinstance(amount, int)):
-            raise PurchaseError('`amount` must be of type int.')
-
         if amount < 1:
             raise PurchaseError('Must purchase at least 1 item.')
-
-        if (timestamp is None) or (not isinstance(timestamp, datetime)):
-            raise PurchaseError('`timestamp` must be of type datetime.datetime')
 
         self.item = item
         self.amount = amount
         self.timestamp = timestamp
-        self.finished = False
 
     def increase_amount(self):
-        if self.finished:
-            raise PurchaseError('Purchase already finished.')
-
         self.amount = self.amount + 1
 
     def decrease_amount(self):
-        if self.finished:
-            raise PurchaseError('Purchase already finished.')
-
         if self.amount == 1:
             raise PurchaseError('Item amount cannot be less than 1.')
 
         self.amount = self.amount - 1
-
-    def finish(self):
-        self.finished = True
 
 
 class PurchaseBasketError(Exception):
@@ -107,27 +69,18 @@ class PurchaseBasket(object):
 
     def add_item(self, item):
         if item.sku in self.purchases:
-            raise PurchaseBasketError('Cannot add an existing item.')
+            self.purchases[item.sku].increase_amount()
+        else:
+            self.purchases[item.sku] = Purchase(item, 1, datetime.utcnow())
 
-        self.purchases[item.sku] = Purchase(item, 1, datetime.utcnow())
-
-    def increase_item_amount(self, sku):
-        if sku not in self.purchases:
-            raise PurchaseBasketError('Cannot increase amount of non-existing item.')
-
-        self.purchases[sku].increase_amount()
-
-    def decrease_item_amount(self, sku):
-        if sku not in self.purchases:
-            raise PurchaseBasketError('Cannot decrease amount of non-existing item.')
-
-        self.purchases[sku].decrease_amount()
-
-    def remove_item(self, sku):
+    def remove_item(self, sku, remove_all=False):
         if sku not in self.purchases:
             raise PurchaseBasketError('Cannot remove non-existing item.')
 
-        del self.purchases[sku]
+        if remove_all or self.purchases[sku].amount == 1:
+            del self.purchases[sku]
+        else:
+            self.purchases[sku].amount = self.purchases[sku].amount - 1
 
     def clear(self):
         if not self.purchases:
--- services/business/test/test_domains.py
+++ services/business/test/test_domains.py
@@ -13,122 +13,35 @@ from app.domains import PurchaseBasketError
 class ItemTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.sku = 'item_sku'
-        self.name = 'item_name'
-        self.price = ('usd', '1.99')
+        self.sku = 'ipd'
+        self.name = 'Super iPad'
+        self.price = '549.99'
+
+    def test_should_not_create_an_item_with_price_in_wrong_format(self):
+        with self.assertRaises(ItemError) as catched:
+            Item(self.sku, self.name, '1.9')
+
+        error = catched.exception
+        self.assertEqual(error.message, 'Amount 1.9 is not in the correct format.')
 
     def test_should_create_an_item(self):
         item = Item(self.sku, self.name, self.price)
 
         self.assertEqual(item.sku, self.sku)
         self.assertEqual(item.name, self.name)
-        self.assertEqual(item.price.currency, self.price[0].upper())
-        self.assertEqual(item.price.amount, Decimal(self.price[1]))
-
-    def test_should_not_create_an_item_with_None_sku(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(None, self.name, self.price)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`sku` must be a string')
-
-    def test_should_not_create_an_item_with_non_string_sku(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(1, self.name, self.price)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`sku` must be a string')
-
-    def test_should_not_create_an_item_with_None_name(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, None, self.price)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`name` must be a string')
-
-    def test_should_not_create_an_item_with_non_string_name(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, 1, self.price)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`name` must be a string')
-
-    def test_should_not_create_an_item_with_None_currency(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, self.name, (None, self.price[1]))
-
-        error = catched.exception
-        self.assertEqual(error.message, '`price[0]` must be a string')
-
-    def test_should_not_create_an_item_with_non_string_currency(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, self.name, (1, self.price[1]))
-
-        error = catched.exception
-        self.assertEqual(error.message, '`price[0]` must be a string')
-
-    def test_should_not_create_an_item_with_None_amount(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, self.name, (self.price[0], None))
-
-        error = catched.exception
-        self.assertEqual(error.message, '`price[1]` must be a string')
-
-    def test_should_not_create_an_item_with_non_string_amount(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, self.name, (self.price[0], 1))
-
-        error = catched.exception
-        self.assertEqual(error.message, '`price[1]` must be a string')
-
-    def test_should_not_create_an_item_with_amount_in_wrong_format(self):
-        with self.assertRaises(ItemError) as catched:
-            Item(self.sku, self.name, (self.price[0], '1.9'))
-
-        error = catched.exception
-        self.assertEqual(error.message, 'Amount 1.9 is not in the correct format.')
+        self.assertEqual(item.price.amount, Decimal(self.price))
 
     def test_should_describe_an_item(self):
         item = Item(self.sku, self.name, self.price)
-        self.assertEqual(item.describe(), '%s %s $%s' % (self.sku, self.name, self.price[1]))
+        self.assertEqual(item.describe(), '%s %s $%s' % (self.sku, self.name, self.price))
 
 
 class PurchaseTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.sku = 'item_sku'
-        self.name = 'item_name'
-        self.price = ('usd', '1.99')
-
-    def test_should_not_purchase_a_None_item(self):
-        with self.assertRaises(PurchaseError) as catched:
-            purchase = Purchase(None, 1, datetime.utcnow())
-
-        error = catched.exception
-        self.assertEqual(error.message, '`item` must be of type `Item`.')
-
-    def test_should_not_purchase_an_item_of_another_class_than_Item(self):
-        with self.assertRaises(PurchaseError) as catched:
-            purchase = Purchase(1, 1, datetime.utcnow())
-
-        error = catched.exception
-        self.assertEqual(error.message, '`item` must be of type `Item`.')
-
-    def test_should_not_purchase_an_item_with_None_amount(self):
-        with self.assertRaises(PurchaseError) as catched:
-            item = Item(self.sku, self.name, self.price)
-            purchase = Purchase(item, None, datetime.utcnow())
-
-        error = catched.exception
-        self.assertEqual(error.message, '`amount` must be of type int.')
-
-    def test_should_not_purchase_an_item_with_non_integer_amount(self):
-        with self.assertRaises(PurchaseError) as catched:
-            item = Item(self.sku, self.name, self.price)
-            purchase = Purchase(item, '1', datetime.utcnow())
-
-        error = catched.exception
-        self.assertEqual(error.message, '`amount` must be of type int.')
+        self.sku = 'ipd'
+        self.name = 'Super iPad'
+        self.price = '549.99'
 
     def test_should_not_purchase_less_than_one_item(self):
         with self.assertRaises(PurchaseError) as catched:
@@ -138,35 +51,18 @@ class PurchaseTestCase(unittest.TestCase):
         error = catched.exception
         self.assertEqual(error.message, 'Must purchase at least 1 item.')
 
-    def test_should_not_purchase_with_None_timestamp(self):
-        with self.assertRaises(PurchaseError) as catched:
-            item = Item(self.sku, self.name, self.price)
-            purchase = Purchase(item, 1, None)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`timestamp` must be of type datetime.datetime')
-
-    def test_should_not_purchase_with_non_datetime_timestamp(self):
-        with self.assertRaises(PurchaseError) as catched:
-            item = Item(self.sku, self.name, self.price)
-            purchase = Purchase(item, 1, 1)
-
-        error = catched.exception
-        self.assertEqual(error.message, '`timestamp` must be of type datetime.datetime')
-
     def test_should_purchase_an_item(self):
         now = datetime.utcnow()
 
         item = Item(self.sku, self.name, self.price)
         purchase = Purchase(item, 3, now)
 
-        clone = Item(self.sku, self.name, self.price)
+        self.assertEqual(purchase.item.sku, self.sku)
+        self.assertEqual(purchase.item.name, self.name)
+        self.assertEqual(purchase.item.price.amount, Decimal(self.price))
 
-        self.assertEqual(purchase.item.sku, clone.sku)
-        self.assertEqual(purchase.item.name, clone.name)
-        self.assertEqual(purchase.item.price.amount, clone.price.amount)
-        self.assertEqual(purchase.item.price.currency, clone.price.currency)
         self.assertEqual(purchase.amount, 3)
+
         self.assertEqual(purchase.timestamp.year, now.year)
         self.assertEqual(purchase.timestamp.month, now.month)
         self.assertEqual(purchase.timestamp.day, now.day)
@@ -174,21 +70,6 @@ class PurchaseTestCase(unittest.TestCase):
         self.assertEqual(purchase.timestamp.minute, now.minute)
         self.assertEqual(purchase.timestamp.second, now.second)
         self.assertEqual(purchase.timestamp.microsecond, now.microsecond)
-        self.assertFalse(purchase.finished)
-
-    def test_should_not_increase_item_amount_for_finished_purchase(self):
-        now = datetime.utcnow()
-
-        item = Item(self.sku, self.name, self.price)
-        purchase = Purchase(item, 3, now)
-
-        purchase.finish()
-
-        with self.assertRaises(PurchaseError) as catched:
-            purchase.increase_amount()
-
-        error = catched.exception
-        self.assertEqual(error.message, 'Purchase already finished.')
 
     def test_should_increase_item_amount_by_one(self):
         now = datetime.utcnow()
@@ -196,23 +77,11 @@ class PurchaseTestCase(unittest.TestCase):
         item = Item(self.sku, self.name, self.price)
         purchase = Purchase(item, 3, now)
 
+        self.assertEqual(purchase.amount, 3)
+
         purchase.increase_amount()
 
         self.assertEqual(purchase.amount, 4)
-
-    def test_should_not_decrease_item_amount_for_finished_purchase(self):
-        now = datetime.utcnow()
-
-        item = Item(self.sku, self.name, self.price)
-        purchase = Purchase(item, 3, now)
-
-        purchase.finish()
-
-        with self.assertRaises(PurchaseError) as catched:
-            purchase.decrease_amount()
-
-        error = catched.exception
-        self.assertEqual(error.message, 'Purchase already finished.')
 
     def test_should_not_decrease_item_amount_to_less_than_one(self):
         now = datetime.utcnow()
@@ -231,6 +100,8 @@ class PurchaseTestCase(unittest.TestCase):
         item = Item(self.sku, self.name, self.price)
         purchase = Purchase(item, 3, now)
 
+        self.assertEqual(purchase.amount, 3)
+
         purchase.decrease_amount()
 
         self.assertEqual(purchase.amount, 2)
@@ -240,87 +111,55 @@ class PurchaseBasketTestCase(unittest.TestCase):
 
     def setUp(self):
         self.purchase_basket = PurchaseBasket()
-        self.item = Item('ipd', 'Super iPad', ('usd', '549.99'))
+        self.item = Item('ipd', 'Super iPad', '549.99')
 
-    def test_should_not_add_existing_item(self):
-        self.purchase_basket.add_item(self.item)
-
-        self.assertEqual(len(self.purchase_basket.purchases), 1)
-
-        with self.assertRaises(PurchaseBasketError) as catched:
-            self.purchase_basket.add_item(self.item)
-
-        error = catched.exception
-        self.assertEqual(error.message, 'Cannot add an existing item.')
-
-    def test_should_add_new_item(self):
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
+    def test_should_add_a_new_item(self):
+        self.assertNotIn(self.item.sku, self.purchase_basket.purchases)
 
         self.purchase_basket.add_item(self.item)
 
-        self.assertEqual(len(self.purchase_basket.purchases), 1)
         self.assertIn(self.item.sku, self.purchase_basket.purchases)
+        self.assertIsNotNone(self.purchase_basket.purchases[self.item.sku])
+        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
 
-    def test_should_not_increase_item_amount_for_non_existing_item(self):
-        with self.assertRaises(PurchaseBasketError) as catched:
-            self.purchase_basket.increase_item_amount(self.item.sku)
-
-        error = catched.exception
-        self.assertEqual(error.message, 'Cannot increase amount of non-existing item.')
-
-    def test_should_increase_item_amount(self):
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
+    def test_should_increment_amount_by_one_of_existing_item(self):
+        self.purchase_basket.purchases[self.item.sku] = Purchase(self.item, 1, datetime.utcnow())
+        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
 
         self.purchase_basket.add_item(self.item)
 
-        self.assertEqual(len(self.purchase_basket.purchases), 1)
-        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
-
-        self.purchase_basket.increase_item_amount(self.item.sku)
-
         self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 2)
 
-    def test_should_not_decrease_item_amount_for_non_existing_item(self):
-        with self.assertRaises(PurchaseBasketError) as catched:
-            self.purchase_basket.decrease_item_amount(self.item.sku)
+    def test_should_not_remove_item_for_non_existing_given_sku(self):
+        self.assertNotIn(self.item.sku, self.purchase_basket.purchases)
 
-        error = catched.exception
-        self.assertEqual(error.message, 'Cannot decrease amount of non-existing item.')
-
-    def test_should_decrease_item_amount(self):
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
-
-        self.purchase_basket.add_item(self.item)
-
-        self.assertEqual(len(self.purchase_basket.purchases), 1)
-        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
-
-        self.purchase_basket.increase_item_amount(self.item.sku)
-
-        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 2)
-
-        self.purchase_basket.decrease_item_amount(self.item.sku)
-
-        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
-
-    def test_should_not_remove_all_items_for_non_existing_given_sku(self):
         with self.assertRaises(PurchaseBasketError) as catched:
             self.purchase_basket.remove_item(self.item.sku)
 
         error = catched.exception
         self.assertEqual(error.message, 'Cannot remove non-existing item.')
 
-    def test_should_remove_all_items_for_given_sku(self):
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
-
+    def test_should_delete_item_when_removing_purchase_of_amount_one(self):
         self.purchase_basket.add_item(self.item)
 
         self.assertIn(self.item.sku, self.purchase_basket.purchases)
 
         self.purchase_basket.remove_item(self.item.sku)
 
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
         self.assertNotIn(self.item.sku, self.purchase_basket.purchases)
+
+    def test_should_decrease_item_amount_when_removing_purchase_of_amount_higher_than_one(self):
+        self.purchase_basket.add_item(self.item)
+
+        self.assertIn(self.item.sku, self.purchase_basket.purchases)
+
+        self.purchase_basket.add_item(self.item)
+
+        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 2)
+
+        self.purchase_basket.remove_item(self.item.sku)
+
+        self.assertEqual(self.purchase_basket.purchases[self.item.sku].amount, 1)
 
     def test_should_not_clear_empty_purchase_basket(self):
         with self.assertRaises(PurchaseBasketError) as catched:
@@ -330,10 +169,13 @@ class PurchaseBasketTestCase(unittest.TestCase):
         self.assertEqual(error.message, 'Cannot clear empty basket.')
 
     def test_should_clear_purchase_basket(self):
+        self.assertFalse(self.purchase_basket.purchases)
+
         self.purchase_basket.add_item(self.item)
 
+        self.assertTrue(self.purchase_basket.purchases)
         self.assertIn(self.item.sku, self.purchase_basket.purchases)
 
         self.purchase_basket.purchases.clear()
 
-        self.assertEqual(len(self.purchase_basket.purchases), 0)
+        self.assertFalse(self.purchase_basket.purchases)
